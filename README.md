e5f885c2521b65463ef9558529b79d0351d2a5fe
zjl233 <1436305806@qq.com>
2019-10-19 20:06:29 +0800

    :page_facing_up: Add reference list.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# moe
+# Moe
 
 a moe language implement by rply
 
+## Reference
+
+- https://github.com/tdsmith/dummylang
--- src/main.py
+++ src/main.py
@@ -2,25 +2,25 @@ from src.lexer import lex
 from src.parser import parse
 
 
-def repl():
-    while True:
-        try:
-            user_input = input('>> ').strip()
-        except EOFError:
-            break
-
-        if user_input:
-            tokens = lex(user_input)
-            tree = parse(tokens)
-            print(tree.eval({}))
-
-
-def test_print():
-    prog = 'print(6)'
-    tokens = lex(prog)
-    print(list(tokens))
-    tree = parse(tokens)
-    print(tree.eval({}))
+# def repl():
+#     while True:
+#         try:
+#             user_input = input('>> ').strip()
+#         except EOFError:
+#             break
+#
+#         if user_input:
+#             tokens = lex(user_input)
+#             tree = parse(tokens)
+#             print(tree.eval({}))
+#
+#
+# def test_print():
+#     prog = 'print(6)'
+#     tokens = lex(prog)
+#     print(list(tokens))
+#     tree = parse(tokens)
+#     print(tree.eval({}))
 
 
 def test_prog():
@@ -36,9 +36,9 @@ def test_prog():
     tree.eval({})
 
 
-def main(prog: str) -> None:
-    tokens = lex(prog)
-    tree = parse(tokens)
+# def main(prog: str) -> None:
+#     tokens = lex(prog)
+#     tree = parse(tokens)
 
 
 if __name__ == '__main__':
